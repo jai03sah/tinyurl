16339db1c3d1099cb227747ed74558503400a3c9
jai03sah <jaiprakashsah57@gmail.com>
2026-02-13 20:35:04 +0530

    1

---
+++ src/main/java/com/tinyrul/tinyurl/shortner/TinyurlShortnerApplication.java
@@ -0,0 +1,13 @@
+package com.tinyrul.tinyurl.shortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyurlShortnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyurlShortnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinyrul/tinyurl/shortner/TinyurlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinyrul.tinyurl.shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyurlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
